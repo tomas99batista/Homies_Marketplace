2e9196eec80ade439f53e5f98723aa847bbaebd3
tomas99batista <tomas99batista@gmail.com>
2020-06-02 20:32:41 +0100

    finished leds and buzzer alarm

--- projRPi/build_status.py
+++ projRPi/build_status.py
@@ -1,18 +1,40 @@
 import requests
 import json
+import time
+from datetime import datetime
+import random
+import RPi.GPIO as GPIO
 
-build_possible_status = ['passing', 'failing']
 
-build_status = ""
+import sensors
 
-response = requests.get("https://api.github.com/repos/tomas99batista/Homies_Marketplace/actions/workflows")
+def get_build_status():
+    build_possible_status = ['failing', 'passing']
 
-json_response = json.loads(response.text)
-badge = json_response['workflows'][1]['badge_url']
+    build_status = ""
 
-badge_response = requests.get("https://github.com/tomas99batista/Homies_Marketplace/workflows/Java%20CI%20with%20Maven/badge.svg")
+    response = requests.get("https://api.github.com/repos/tomas99batista/Homies_Marketplace/actions/workflows")
 
-for b in build_possible_status:
-    if b in badge_response.text:
-        build_status = b
+    json_response = json.loads(response.text)
+    badge = json_response['workflows'][1]['badge_url']
 
+    badge_response = requests.get("https://github.com/tomas99batista/Homies_Marketplace/workflows/Java%20CI%20with%20Maven/badge.svg")
+
+    for b in build_possible_status:
+        if b in badge_response.text:
+            build_status = b
+
+    return build_status
+
+while True:
+    try:
+        # This can be used to tests:
+        #build_possible_status = ['passing', 'failing']
+        #build_status = random.choice(build_possible_status)
+        
+        build_status = get_build_status()
+        print("BUILD_STATUS:\t", build_status.upper(), '\t', datetime.now())
+        sensors.turn_led_build_status(build_status)
+        time.sleep(30)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
