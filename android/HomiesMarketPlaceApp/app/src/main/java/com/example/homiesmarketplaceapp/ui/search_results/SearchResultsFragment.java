8de627e091215130b0deeae08580544e877dfd8d
jose <josefrias99@ua.pt>
2020-06-04 19:18:22 +0100

    android minor changes

--- android/HomiesMarketPlaceApp/app/src/main/java/com/example/homiesmarketplaceapp/ui/search_results/SearchResultsFragment.java
+++ android/HomiesMarketPlaceApp/app/src/main/java/com/example/homiesmarketplaceapp/ui/search_results/SearchResultsFragment.java
@@ -56,7 +56,7 @@ public class SearchResultsFragment extends Fragment {
     }
 
     private void search(String city, String type, String minPrice, String maxPrice, String bedrooms, String bathrooms, String rating){
-        String initialUrl="http://10.0.2.2:8080/api/search/";
+        String initialUrl="http://192.168.160.30:8080/api/search/";
         String searchUrl="";
         if (city!=null && !city.equals("")){
             searchUrl+="?city="+city;
